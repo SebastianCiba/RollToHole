85ecf5ce3b4305da36f44e6e8f51b0e75c058895
Sebastian Ciba <sebastian.ciba@o2.pl>
2018-12-30 03:31:45 +0100

    0.14

--- Assets/_Completed-Game/Scripts/PlayerController.cs
+++ Assets/_Completed-Game/Scripts/PlayerController.cs
@@ -3,12 +3,14 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    private float push = 140;
-    private float pull = -140;
-    private bool touch = false;
     private int move = 0;
-    private int round = 0;
     private AudioSource source;
+    private Rounds rounds;
+
+    public float push = 130;
+    protected float pull = -130;
+    protected bool touch = false;
+    protected int round = 1;
 
     public Rigidbody rb;
     public Text winText;
@@ -17,7 +19,6 @@ public class PlayerController : MonoBehaviour
     public Button ButtonExit;
     public Button ButtonTryAgain;
     public RawImage Background;
-    public GameObject exit;
 
     //public List<Vector3> roundOne = new List<Vector3>();
     //public List<GameObject> cubes = new List<GameObject>();
@@ -27,13 +28,14 @@ public class PlayerController : MonoBehaviour
     {
         //initObjectRound();
         rb = GetComponent<Rigidbody>();
-        rb.Sleep();
+        rounds = GetComponent<Rounds>();
         winText.text = "";
         moveText.text = "Moves: 0";
 
         Button ButtonStart1 = ButtonStart.GetComponent<Button>();
         Button ButtonExit1 = ButtonExit.GetComponent<Button>();
         Button ButtonTryAgain1 = ButtonTryAgain.GetComponent<Button>();
+        source = GetComponent<AudioSource>();
 
         ButtonStart1.onClick.AddListener(TaskOnClickStart);
         ButtonExit1.onClick.AddListener(TaskOnClickExit);
@@ -41,9 +43,8 @@ public class PlayerController : MonoBehaviour
 
         ButtonStart.gameObject.SetActive(true);
         Background.gameObject.SetActive(true);
-        source = GetComponent<AudioSource>();
+        
     }
-
     //kamilowe rzeczy
     //void initObjectRound()
     //{
@@ -59,152 +60,27 @@ public class PlayerController : MonoBehaviour
     //   //     gameObject.AddComponent("Cube");
     //       // temporary.MovePosition(temp);
     //  //  }
-    //}
-
-    void RoundOne()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube[i].tag = "CollisionBox";
-            cube[i].name = "cube_" + i;
-        }
-
-        cube[0].transform.position = new Vector3(4, 0.5f, 0);
-        cube[1].transform.position = new Vector3(-6, 0.5f, 0);
-        cube[2].transform.position = new Vector3(1, 0.5f, -9);
-        cube[3].transform.position = new Vector3(0, 0.5f, -13);
-        cube[4].transform.position = new Vector3(3, 0.5f, -14);
-
-        rb.transform.position = new Vector3(0, 1, 0);
-        //rb.Sleep();
-        touch = true;
-    }
-
-    void RoundTwo()
-    {
-        push += 10;
-        pull -= 10;
-        exit.transform.position = new Vector3(0, 0, 3);
-
-        for (int i = 0; i < 23; i++)
-        {
-            cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube[i].tag = "CollisionBox";
-            cube[i].name = "cube_" + i;
-        }
-
-        cube[0].transform.position = new Vector3(5, 0.5f, 0);
-        cube[1].transform.position = new Vector3(4, 0.5f, -3);
-        cube[2].transform.position = new Vector3(0, 0.5f, -2);
-        cube[3].transform.position = new Vector3(1, 0.5f, 2);
-        cube[4].transform.position = new Vector3(-2, 0.5f, 1);
-        cube[5].transform.position = new Vector3(-1, 0.5f, -4);
-        cube[6].transform.position = new Vector3(3, 0.5f, -6);
-        cube[7].transform.position = new Vector3(-4, 0.5f, -5);
-        cube[8].transform.position = new Vector3(-3, 0.5f, 2);
-        cube[9].transform.position = new Vector3(-6, 0.5f, 1);
-        cube[10].transform.position = new Vector3(-5, 0.5f, 3);
-        cube[11].transform.position = new Vector3(-8, 0.5f, 2);
-        cube[12].transform.position = new Vector3(-7, 0.5f, 5);
-        cube[13].transform.position = new Vector3(3, 0.5f, 4);
-        cube[14].transform.position = new Vector3(2, 0.5f, -8);
-        cube[15].transform.position = new Vector3(2, 0.5f, 6);
-        cube[16].transform.position = new Vector3(6, 0.5f, 5);
-        cube[17].transform.position = new Vector3(-7, 0.5f, 0);
-        cube[18].transform.position = new Vector3(-5, 0.5f, -1);
-        cube[19].transform.position = new Vector3(-4, 0.5f, -3);
-        cube[20].transform.position = new Vector3(-6, 0.5f, -4);
-        cube[21].transform.position = new Vector3(-8, 0.5f, -8);
-        cube[22].transform.position = new Vector3(-3, 0.5f, -9);
-
-        rb.transform.position = new Vector3(0, 1, 0);
-        rb.Sleep();
-        touch = true;
-    }
-
-    void RoundThree()
-    {
-        push += 10;
-        pull -= 10;
-        exit.transform.position = new Vector3(0, 0, 0);
-        for (int i = 0; i < 32; i++)
-        {
-            cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube[i].tag = "CollisionBox";
-            cube[i].name = "cube_" + i;
-        }
-
-        cube[0].transform.position = new Vector3(0, 0.5f, -2);
-        cube[1].transform.position = new Vector3(6, 0.5f, -1);
-        cube[2].transform.position = new Vector3(5, 0.5f, -4);
-        cube[3].transform.position = new Vector3(-5, 0.5f, -3);
-        cube[4].transform.position = new Vector3(-4, 0.5f, 3);
-        cube[5].transform.position = new Vector3(-6, 0.5f, 2);
-        cube[6].transform.position = new Vector3(-5, 0.5f, 8);
-        cube[7].transform.position = new Vector3(-8, 0.5f, 7);
-        cube[8].transform.position = new Vector3(-7, 0.5f, -6);
-        cube[9].transform.position = new Vector3(-4, 0.5f, -5);
-        cube[10].transform.position = new Vector3(-5, 0.5f, -8);
-        cube[11].transform.position = new Vector3(-10, 0.5f, -7);
-        cube[12].transform.position = new Vector3(-9, 0.5f, 10);
-        cube[13].transform.position = new Vector3(4, 0.5f, 9);
-
-        cube[14].transform.position = new Vector3(-4, 0.5f, 8);
-        cube[15].transform.position = new Vector3(9, 0.5f, 8);
-        cube[16].transform.position = new Vector3(-1, 0.5f, 6);
-        cube[17].transform.position = new Vector3(1, 0.5f, 6);
-        cube[18].transform.position = new Vector3(-4, 0.5f, 5);
-        cube[19].transform.position = new Vector3(5, 0.5f, 5);
-        cube[20].transform.position = new Vector3(0, 0.5f, 4);
-        cube[21].transform.position = new Vector3(9, 0.5f, 3);
-        cube[22].transform.position = new Vector3(2, 0.5f, 2);
-        cube[23].transform.position = new Vector3(7, 0.5f, 1);
-        cube[24].transform.position = new Vector3(-2, 0.5f, 0);
-        cube[25].transform.position = new Vector3(2, 0.5f, 0);
-        cube[26].transform.position = new Vector3(8, 0.5f, -3);
-        cube[27].transform.position = new Vector3(7, 0.5f, -5);
-        cube[28].transform.position = new Vector3(1, 0.5f, -6);
-        cube[29].transform.position = new Vector3(-3, 0.5f, -9);
-        cube[30].transform.position = new Vector3(6, 0.5f, -9);
-
-        cube[31].transform.position = new Vector3(3, 0.5f, -11);
-
-        rb.transform.position = new Vector3(0, 1, 0);
-        rb.Sleep();
-        touch = true;
-    }
-
+    //}   
     void TaskOnClickStart()
     {
-        round++;
         move = 0;
-        for (int i = 0; i < cube.Length; i++)
-        {
-            Destroy(cube[i]);
-        }
+        moveText.text = "Moves: " + move.ToString();
         winText.text = "";
 
         ButtonStart.gameObject.SetActive(false);
         Background.gameObject.SetActive(false);
-        rb.AddForce(0, 30, 0, ForceMode.Impulse);
-        //rb.Sleep();
-        //touch = true;
-        switch (round)
-        {
-            case 1:
-                RoundOne();
-                break;
-            case 2:
-                RoundTwo();
-                break;
-            case 3:
-                RoundThree();
-                break;
-            default:
-                Background.gameObject.SetActive(true);
-                break;
-        }
+        rounds.StartRound();
+    }
+
+    void TaskOnClickTryAgain()
+    {
+        move = 0;
+        moveText.text = "Moves: " + move.ToString();
+        winText.text = "";
+
+        ButtonTryAgain.gameObject.SetActive(false);
+        Background.gameObject.SetActive(false);
+        rounds.StartRound();
     }
 
     void TaskOnClickExit()
@@ -212,30 +88,6 @@ public class PlayerController : MonoBehaviour
         Application.Quit();
     }
 
-    void TaskOnClickTryAgain()
-    {
-        move = 0;
-        winText.text = "";
-
-        ButtonTryAgain.gameObject.SetActive(false);
-        Background.gameObject.SetActive(false);
-        rb.AddForce(0, 30, 0, ForceMode.Impulse);
-        switch (round)
-        {
-            case 1:
-                RoundOne();
-                break;
-            case 2:
-                RoundTwo();
-                break;
-            case 3:
-                RoundThree();
-                break;
-            default:
-                Background.gameObject.SetActive(true);
-                break;
-        }
-    }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -258,14 +110,19 @@ public class PlayerController : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Finish"))
         {
+            push += 10;
+            pull += 10;
             source.Play();
             winText.text = "You Win!";
             ButtonStart.gameObject.SetActive(true);
             Background.gameObject.SetActive(true);
+            round++;
         }
 
         if (collision.gameObject.CompareTag("Lose"))
         {
+            push -= 10;
+            pull -= 10;
             source.Play();
             winText.text = "You Lose!";
             ButtonTryAgain.gameObject.SetActive(true);
@@ -316,21 +173,6 @@ public class PlayerController : MonoBehaviour
             }
 
         }
-    }
-
-
-    // collision with obj with "Is Trigger" is checked
-    //void OnTriggerEnter(Collider other)
-    //{
-
-    //}
-
-    void OnGUI()
-    {
-        GUI.Box(new Rect(0, 0, 100, 50), moveText.text.ToString());
-        //    GUI.Box(new Rect(Screen.width - 100, 0, 100, 50), "Top-right");
-        //    GUI.Box(new Rect(0, Screen.height - 50, 100, 50), "Bottom-left");
-        //    GUI.Box(new Rect(Screen.width - 100, Screen.height - 50, 100, 50), "Bottom right");
     }
 }
 
---
+++ Assets/_Completed-Game/Scripts/Rounds.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+
+public class Rounds : PlayerController
+{
+    public GameObject exit;
+    public void StartRound()
+    {
+        for (int i = 0; i < cube.Length; i++)
+        {
+            Destroy(cube[i]);
+        }
+
+        switch (round)
+        {
+            case 1:
+                RoundOne();
+                break;
+            case 2:
+                RoundTwo();
+                break;
+            case 3:
+                RoundThree();
+                break;
+            case 4:
+                RoundFour();
+                break;
+                //default:
+                //    Background.gameObject.SetActive(true);
+                //    break;
+        }
+    }
+
+    public void RoundOne()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube[i].tag = "CollisionBox";
+            cube[i].name = "cube_" + i;
+        }
+
+        cube[0].transform.position = new Vector3(4, 0.5f, 0);
+        cube[1].transform.position = new Vector3(-6, 0.5f, 0);
+        cube[2].transform.position = new Vector3(1, 0.5f, -9);
+        cube[3].transform.position = new Vector3(0, 0.5f, -13);
+        cube[4].transform.position = new Vector3(3, 0.5f, -14);
+        rb.transform.position = new Vector3(0, 1, 0);
+        touch = true;
+    }
+
+    public void RoundTwo()
+    {
+        exit.transform.position = new Vector3(0, 0, 3);
+
+        for (int i = 0; i < 23; i++)
+        {
+            cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube[i].tag = "CollisionBox";
+            cube[i].name = "cube_" + i;
+        }
+
+        cube[0].transform.position = new Vector3(5, 0.5f, 0);
+        cube[1].transform.position = new Vector3(4, 0.5f, -3);
+        cube[2].transform.position = new Vector3(0, 0.5f, -2);
+        cube[3].transform.position = new Vector3(1, 0.5f, 2);
+        cube[4].transform.position = new Vector3(-2, 0.5f, 1);
+        cube[5].transform.position = new Vector3(-1, 0.5f, -4);
+        cube[6].transform.position = new Vector3(3, 0.5f, -6);
+        cube[7].transform.position = new Vector3(-4, 0.5f, -5);
+        cube[8].transform.position = new Vector3(-3, 0.5f, 2);
+        cube[9].transform.position = new Vector3(-6, 0.5f, 1);
+        cube[10].transform.position = new Vector3(-5, 0.5f, 3);
+        cube[11].transform.position = new Vector3(-8, 0.5f, 2);
+        cube[12].transform.position = new Vector3(-7, 0.5f, 5);
+        cube[13].transform.position = new Vector3(3, 0.5f, 4);
+        cube[14].transform.position = new Vector3(2, 0.5f, -8);
+        cube[15].transform.position = new Vector3(2, 0.5f, 6);
+        cube[16].transform.position = new Vector3(6, 0.5f, 5);
+        cube[17].transform.position = new Vector3(-7, 0.5f, 0);
+        cube[18].transform.position = new Vector3(-5, 0.5f, -1);
+        cube[19].transform.position = new Vector3(-4, 0.5f, -3);
+        cube[20].transform.position = new Vector3(-6, 0.5f, -4);
+        cube[21].transform.position = new Vector3(-8, 0.5f, -8);
+        cube[22].transform.position = new Vector3(-3, 0.5f, -9);
+
+        rb.transform.position = new Vector3(0, 1, 0);
+        rb.Sleep();
+        touch = true;
+    }
+
+    public void RoundThree()
+    {
+        exit.transform.position = new Vector3(0, 0, 0);
+        for (int i = 0; i < 32; i++)
+        {
+            cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube[i].tag = "CollisionBox";
+            cube[i].name = "cube_" + i;
+        }
+
+        cube[0].transform.position = new Vector3(0, 0.5f, -2);
+        cube[1].transform.position = new Vector3(6, 0.5f, -1);
+        cube[2].transform.position = new Vector3(5, 0.5f, -4);
+        cube[3].transform.position = new Vector3(-5, 0.5f, -3);
+        cube[4].transform.position = new Vector3(-4, 0.5f, 3);
+        cube[5].transform.position = new Vector3(-6, 0.5f, 2);
+        cube[6].transform.position = new Vector3(-5, 0.5f, 8);
+        cube[7].transform.position = new Vector3(-8, 0.5f, 7);
+        cube[8].transform.position = new Vector3(-7, 0.5f, -6);
+        cube[9].transform.position = new Vector3(-4, 0.5f, -5);
+        cube[10].transform.position = new Vector3(-5, 0.5f, -8);
+        cube[11].transform.position = new Vector3(-10, 0.5f, -7);
+        cube[12].transform.position = new Vector3(-9, 0.5f, 10);
+        cube[13].transform.position = new Vector3(4, 0.5f, 9);
+
+        cube[14].transform.position = new Vector3(-4, 0.5f, 8);
+        cube[15].transform.position = new Vector3(9, 0.5f, 8);
+        cube[16].transform.position = new Vector3(-1, 0.5f, 6);
+        cube[17].transform.position = new Vector3(1, 0.5f, 6);
+        cube[18].transform.position = new Vector3(-4, 0.5f, 5);
+        cube[19].transform.position = new Vector3(5, 0.5f, 5);
+        cube[20].transform.position = new Vector3(0, 0.5f, 4);
+        cube[21].transform.position = new Vector3(9, 0.5f, 3);
+        cube[22].transform.position = new Vector3(2, 0.5f, 2);
+        cube[23].transform.position = new Vector3(7, 0.5f, 1);
+        cube[24].transform.position = new Vector3(-2, 0.5f, 0);
+        cube[25].transform.position = new Vector3(2, 0.5f, 0);
+        cube[26].transform.position = new Vector3(8, 0.5f, -3);
+        cube[27].transform.position = new Vector3(7, 0.5f, -5);
+        cube[28].transform.position = new Vector3(1, 0.5f, -6);
+        cube[29].transform.position = new Vector3(-3, 0.5f, -9);
+        cube[30].transform.position = new Vector3(6, 0.5f, -9);
+
+        cube[31].transform.position = new Vector3(3, 0.5f, -11);
+
+        rb.transform.position = new Vector3(0, 1, 0);
+        rb.Sleep();
+        touch = true;
+    }
+
+    public void RoundFour()
+    {
+        exit.transform.position = new Vector3(0, 0, 0);
+        for (int i = 0; i < 32; i++)
+        {
+            cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube[i].tag = "CollisionBox";
+            cube[i].name = "cube_" + i;
+        }
+
+        cube[0].transform.position = new Vector3(0, 0.5f, -2);
+        cube[1].transform.position = new Vector3(6, 0.5f, -1);
+        cube[2].transform.position = new Vector3(5, 0.5f, -4);
+        cube[3].transform.position = new Vector3(-5, 0.5f, -3);
+        cube[4].transform.position = new Vector3(-4, 0.5f, 3);
+        cube[5].transform.position = new Vector3(-6, 0.5f, 2);
+        cube[6].transform.position = new Vector3(-5, 0.5f, 8);
+        cube[7].transform.position = new Vector3(-8, 0.5f, 7);
+        cube[8].transform.position = new Vector3(-7, 0.5f, -6);
+        cube[9].transform.position = new Vector3(-4, 0.5f, -5);
+        cube[10].transform.position = new Vector3(-5, 0.5f, -8);
+        cube[11].transform.position = new Vector3(-10, 0.5f, -7);
+        cube[12].transform.position = new Vector3(-9, 0.5f, 10);
+        cube[13].transform.position = new Vector3(4, 0.5f, 9);
+
+        cube[14].transform.position = new Vector3(-4, 0.5f, 8);
+        cube[15].transform.position = new Vector3(9, 0.5f, 8);
+        cube[16].transform.position = new Vector3(-1, 0.5f, 6);
+        cube[17].transform.position = new Vector3(1, 0.5f, 6);
+        cube[18].transform.position = new Vector3(-4, 0.5f, 5);
+        cube[19].transform.position = new Vector3(5, 0.5f, 5);
+        cube[20].transform.position = new Vector3(0, 0.5f, 4);
+        cube[21].transform.position = new Vector3(9, 0.5f, 3);
+        cube[22].transform.position = new Vector3(2, 0.5f, 2);
+        cube[23].transform.position = new Vector3(7, 0.5f, 1);
+        cube[24].transform.position = new Vector3(-2, 0.5f, 0);
+        cube[25].transform.position = new Vector3(2, 0.5f, 0);
+        cube[26].transform.position = new Vector3(8, 0.5f, -3);
+        cube[27].transform.position = new Vector3(7, 0.5f, -5);
+        cube[28].transform.position = new Vector3(1, 0.5f, -6);
+        cube[29].transform.position = new Vector3(-3, 0.5f, -9);
+        cube[30].transform.position = new Vector3(6, 0.5f, -9);
+
+        cube[31].transform.position = new Vector3(3, 0.5f, -11);
+
+        rb.transform.position = new Vector3(0, 1, 0);
+        rb.Sleep();
+        touch = true;
+    }
+}
