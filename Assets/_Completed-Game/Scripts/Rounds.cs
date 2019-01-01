4abb7eaf2c3e2c992b9b0ea51f24b8b4019fb7a2
Sebastian Ciba <sebastian.ciba@o2.pl>
2019-01-01 12:24:39 +0100

    0.18

--- Assets/_Completed-Game/Scripts/Rounds.cs
+++ Assets/_Completed-Game/Scripts/Rounds.cs
@@ -6,9 +6,14 @@ public class Rounds : PlayerController
     public GameObject spikes;
     public UnityEngine.UI.RawImage Win;
     public Material Material1;
+    public Material Material2;
+    public Material Material3;
+    public Material Material4;
+    public Material Material5;
 
     public void StartRound()
     {
+        round = 5;
         spikes.GetComponent<Renderer>().material.color = Color.red;
         
         for (int i = 0; i < cube.Length; i++)
@@ -30,6 +35,9 @@ public class Rounds : PlayerController
             case 4:
                 RoundFour();
                 break;
+            case 5:
+                RoundFive();
+                break;
             default:
                 Win.gameObject.SetActive(true);
                 break;
@@ -38,13 +46,15 @@ public class Rounds : PlayerController
 
     public void RoundOne()
     {
-        //Object.GetComponent<MeshRenderer>().material = Material1;
+        rb.transform.position = new Vector3(0, 1, 0);
+        touch = true;
+
         for (int i = 0; i < 5; i++)
         {
             cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube[i].tag = "CollisionBox";
             cube[i].name = "cube_" + i;
-            cube[i].GetComponent<Renderer>().material.color = Color.blue;
+            cube[i].GetComponent<MeshRenderer>().material = Material1;
         }
 
         cube[0].transform.position = new Vector3(4, 0.5f, 0);
@@ -52,20 +62,21 @@ public class Rounds : PlayerController
         cube[2].transform.position = new Vector3(1, 0.5f, -9);
         cube[3].transform.position = new Vector3(0, 0.5f, -13);
         cube[4].transform.position = new Vector3(3, 0.5f, -14);
-        rb.transform.position = new Vector3(0, 1, 0);
-        touch = true;
     }
 
     public void RoundTwo()
     {
         exit.transform.position = new Vector3(0, 0, 3);
+        rb.transform.position = new Vector3(0, 1, 0);
+        rb.Sleep();
+        touch = true;
 
         for (int i = 0; i < 23; i++)
         {
             cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube[i].tag = "CollisionBox";
             cube[i].name = "cube_" + i;
-            cube[i].GetComponent<Renderer>().material.color = Color.red;
+            cube[i].GetComponent<MeshRenderer>().material = Material2;
         }
 
         cube[0].transform.position = new Vector3(5, 0.5f, 0);
@@ -91,22 +102,21 @@ public class Rounds : PlayerController
         cube[20].transform.position = new Vector3(-6, 0.5f, -4);
         cube[21].transform.position = new Vector3(-8, 0.5f, -8);
         cube[22].transform.position = new Vector3(-3, 0.5f, -9);
-
-        rb.transform.position = new Vector3(0, 1, 0);
-        rb.Sleep();
-        touch = true;
     }
 
     public void RoundThree()
     {
         exit.transform.position = new Vector3(0, 0, 0);
+        rb.transform.position = new Vector3(0, 1, 0);
+        rb.Sleep();
+        touch = true;
+
         for (int i = 0; i < 32; i++)
         {
             cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube[i].tag = "CollisionBox";
             cube[i].name = "cube_" + i;
-            //cube[i].GetComponent<Renderer>().material.color = Color.green;
-            cube[i].GetComponent<MeshRenderer>().material = Material1;
+            cube[i].GetComponent<MeshRenderer>().material = Material3;
         }
 
         cube[0].transform.position = new Vector3(0, 0.5f, -2);
@@ -143,10 +153,6 @@ public class Rounds : PlayerController
         cube[30].transform.position = new Vector3(6, 0.5f, -9);
 
         cube[31].transform.position = new Vector3(3, 0.5f, -11);
-
-        rb.transform.position = new Vector3(0, 1, 0);
-        rb.Sleep();
-        touch = true;
     }
 
     public void RoundFour()
@@ -154,12 +160,16 @@ public class Rounds : PlayerController
         spikes.SetActive(true);
         exit.transform.position = new Vector3(0, 0, 0);
         exit.transform.rotation = Quaternion.Euler(0, 180, 0);
+        rb.transform.position = new Vector3(0, 1, 0);
+        rb.Sleep();
+        touch = true;
+
         for (int i = 0; i < 33; i++)
         {
             cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube[i].tag = "CollisionBox";
             cube[i].name = "cube_" + i;
-            cube[i].GetComponent<Renderer>().material.color = Color.cyan;
+            cube[i].GetComponent<MeshRenderer>().material = Material4;
         }
 
         cube[0].transform.position = new Vector3(-2, 0.5f, 0);
@@ -195,9 +205,60 @@ public class Rounds : PlayerController
         cube[30].transform.position = new Vector3(-9, 0.5f, 6);
         cube[31].transform.position = new Vector3(-11, 0.5f, 3);
         cube[32].transform.position = new Vector3(-7, 0.5f, 11);
+    }
 
+    public void RoundFive()
+    {
+        spikes.SetActive(true);
+        exit.transform.position = new Vector3(0, 0, 0);
+        exit.transform.rotation = Quaternion.Euler(0, 180, 0);
         rb.transform.position = new Vector3(0, 1, 0);
         rb.Sleep();
         touch = true;
+
+        for (int i = 0; i < 35; i++)
+        {
+            cube[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube[i].tag = "CollisionBox";
+            cube[i].name = "cube_" + i;
+            cube[i].GetComponent<MeshRenderer>().material = Material5;
+        }
+
+        cube[0].transform.position = new Vector3(0, 0.5f, 2);
+        cube[1].transform.position = new Vector3(3, 0.5f, 1);
+        cube[2].transform.position = new Vector3(2, 0.5f, -3);
+        cube[3].transform.position = new Vector3(-3, 0.5f, -2);
+        cube[4].transform.position = new Vector3(-2, 0.5f, -5);
+        cube[5].transform.position = new Vector3(4, 0.5f, -4);
+        cube[6].transform.position = new Vector3(3, 0.5f, -8);
+        cube[7].transform.position = new Vector3(-4, 0.5f, -7);
+        cube[8].transform.position = new Vector3(-3, 0.5f, -12);
+        cube[9].transform.position = new Vector3(6, 0.5f, -11);
+        cube[10].transform.position = new Vector3(5, 0.5f, 5);
+        cube[11].transform.position = new Vector3(8, 0.5f, 4);
+        cube[12].transform.position = new Vector3(7, 0.5f, 2);
+        cube[13].transform.position = new Vector3(-5, 0.5f, 3);
+        cube[14].transform.position = new Vector3(-4, 0.5f, -2);
+        cube[15].transform.position = new Vector3(-9, 0.5f, -1);
+        cube[16].transform.position = new Vector3(-8, 0.5f, 8);
+        cube[17].transform.position = new Vector3(-5, 0.5f, 7);
+        cube[18].transform.position = new Vector3(-6, 0.5f, 4);
+        cube[19].transform.position = new Vector3(5, 0.5f, 5);
+        cube[20].transform.position = new Vector3(4, 0.5f, 11);
+
+        cube[21].transform.position = new Vector3(10, 0.5f, 0);
+        cube[22].transform.position = new Vector3(9, 0.5f, -10);
+        cube[23].transform.position = new Vector3(-10, 0.5f, -11);
+        cube[24].transform.position = new Vector3(-11, 0.5f, 4);
+        cube[25].transform.position = new Vector3(-10, 0.5f, -6);
+        cube[26].transform.position = new Vector3(-12, 0.5f, 2);
+        cube[27].transform.position = new Vector3(-0, 0.5f, -13);
+        cube[28].transform.position = new Vector3(12, 0.5f, -6);
+        cube[29].transform.position = new Vector3(-13, 0.5f, 7);
+        cube[30].transform.position = new Vector3(8, 0.5f, 10);
+        cube[31].transform.position = new Vector3(7, 0.5f, 12);
+        cube[32].transform.position = new Vector3(-4, 0.5f, 14);
+        cube[33].transform.position = new Vector3(10, 0.5f, 6);
+        cube[34].transform.position = new Vector3(6, 0.5f, 15);
     }
 }
